6b033c8077d87f97cae8c31aac6997b4d0a29740
Jordan Crouse <jcrouse@codeaurora.org>
2013-12-16 14:18:26 -0700

    msm: kgsl: Add a timer to detect possible syncpoint deadlocks
    Use timers to detect when a syncpoint stays active for too long. This
    won't avoid deadlocks but it sure will make it easier to debug them.
    
    Conflicts:
            drivers/gpu/msm/kgsl_device.h
    
    Change-Id: Ic0dedbadde07abcc1d39563c3b141591cbf2f634
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -118,6 +118,7 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	struct sync_pt *pt;
 	struct sync_fence *fence = NULL;
 	int ret = -EINVAL;
+	char fence_name[sizeof(fence->name)] = {};
 
 	priv.fence_fd = -1;
 
@@ -144,8 +145,13 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 		ret = -ENOMEM;
 		goto unlock;
 	}
+	snprintf(fence_name, sizeof(fence_name),
+		"%s-pid-%d-ctx-%d-ts-%d",
+		device->name, current->group_leader->pid,
+		context_id, timestamp);
 
-	fence = sync_fence_create("kgsl-fence", pt);
+
+	fence = sync_fence_create(fence_name, pt);
 	if (fence == NULL) {
 		/* only destroy pt when not added to fence */
 		kgsl_sync_pt_destroy(pt);
