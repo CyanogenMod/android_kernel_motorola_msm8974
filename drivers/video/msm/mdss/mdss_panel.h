6cee40291dc1ede11bc105c43d6b7f5d6ec3eb8c
Jeykumar Sankaran <jsanka@codeaurora.org>
2014-03-06 12:12:24 -0800

    msm: mdss: dsi: Share panel alignment restrictions through sysfs node
    Read panel alignment restrictions on left, top, width and height values of
    the updating ROI from the dtsi file and share through sysfs node.
    Framework needs these details for configuring MDP for certain
    features (e.g. Partial frame update).
    
    Change-Id: Ibe6973b721bb20120a6d7a217bad2d0d159cc836
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -348,6 +348,11 @@ struct mdss_panel_info {
 	int new_fps;
 	int panel_max_fps;
 	int panel_max_vtotal;
+	u32 xstart_pix_align;
+	u32 width_pix_align;
+	u32 ystart_pix_align;
+	u32 height_pix_align;
+
 	u32 cont_splash_enabled;
 	u32 cont_splash_feature_on;
 	bool cont_splash_esd_rdy;
