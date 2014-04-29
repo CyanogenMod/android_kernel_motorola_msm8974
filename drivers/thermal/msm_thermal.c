9516127b9570c574ae891962e42f9fa1481c9d9c
Shiju Mathew <shijum@codeaurora.org>
2014-04-28 20:32:23 -0400

    msm: thermal: Remove D tasks in kernel thermal monitor
    Remove D tasks in kernel thermal monitor by moving
    wait completion to its interruptible counterpart.
    This will reduce the loadavg by 50 percent.
    
    Change-Id: I934bd18d4f0f6b7c59ef4813efa1036a185c4715
    CRs-Fixed: 654474
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1088,7 +1088,9 @@ static __ref int do_hotplug(void *data)
 	}
 
 	while (!kthread_should_stop()) {
-		wait_for_completion(&hotplug_notify_complete);
+		while (wait_for_completion_interruptible(
+			&hotplug_notify_complete) != 0)
+			;
 		INIT_COMPLETION(hotplug_notify_complete);
 		mask = 0;
 
@@ -1539,7 +1541,9 @@ static __ref int do_freq_mitigation(void *data)
 	uint32_t cpu = 0, max_freq_req = 0, min_freq_req = 0;
 
 	while (!kthread_should_stop()) {
-		wait_for_completion(&freq_mitigation_complete);
+		while (wait_for_completion_interruptible(
+			&freq_mitigation_complete) != 0)
+			;
 		INIT_COMPLETION(freq_mitigation_complete);
 
 		for_each_possible_cpu(cpu) {
@@ -1773,7 +1777,9 @@ static __ref int do_thermal_monitor(void *data)
 	struct therm_threshold *sensor_list;
 
 	while (!kthread_should_stop()) {
-		wait_for_completion(&thermal_monitor_complete);
+		while (wait_for_completion_interruptible(
+			&thermal_monitor_complete) != 0)
+			;
 		INIT_COMPLETION(thermal_monitor_complete);
 
 		for (i = 0; i < MSM_LIST_MAX_NR; i++) {
