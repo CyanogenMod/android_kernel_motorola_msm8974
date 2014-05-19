5ee5cef28e39c280ed7d9ac86ed85d51b63182b0
Jayant Shekhar <jshekhar@codeaurora.org>
2014-05-19 13:11:47 +0530

    msm: mdss: IOMMU ref count implementation
    Currently MDP is attaching/detaching iommu based on
    bus bandwidth requests. This can lead to performance
    issues, so implement ref count on iommu such that
    iommu is attached where needed.
    
    Change-Id: Ic4d35e2dc7f83291d1ab93d3e0109a2d69c98844
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>
    Signed-off-by: Vineet Bajaj <vbajaj@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_host.c
    	drivers/video/msm/mdss/mdss_mdp.c
    	drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
    	drivers/video/msm/mdss/mdss_mdp_splash_logo.c

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -192,6 +192,8 @@ static inline void mdss_mdp_cmd_clk_on(struct mdss_mdp_cmd_ctx *ctx)
 {
 	unsigned long flags;
 	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
+	int rc;
+
 	mutex_lock(&ctx->clk_mtx);
 	MDSS_XLOG(ctx->pp_num, ctx->koff_cnt, ctx->clk_enabled,
 						ctx->rdptr_enabled);
@@ -200,6 +202,12 @@ static inline void mdss_mdp_cmd_clk_on(struct mdss_mdp_cmd_ctx *ctx)
 		if (cancel_delayed_work_sync(&ctx->ulps_work))
 			pr_debug("deleted pending ulps work\n");
 
+		rc = mdss_iommu_ctrl(1);
+		if (IS_ERR_VALUE(rc)) {
+			pr_err("IOMMU attach failed\n");
+			mutex_unlock(&ctx->clk_mtx);
+			return;
+		}
 		mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 
 		if (ctx->ulps) {
@@ -242,6 +250,7 @@ static inline void mdss_mdp_cmd_clk_off(struct mdss_mdp_cmd_ctx *ctx)
 		mdss_mdp_hist_intr_setup(&mdata->hist_intr, MDSS_IRQ_SUSPEND);
 		mdss_mdp_ctl_intf_event
 			(ctx->ctl, MDSS_EVENT_PANEL_CLK_CTRL, (void *)0);
+		mdss_iommu_ctrl(0);
 		mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 		if (ctx->panel_on &&
 			ctx->ctl->panel_data->panel_info.ulps_feature_enabled)
