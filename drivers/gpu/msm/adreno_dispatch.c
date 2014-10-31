22f9436aac4087c8fbe7146cd304c06c1671640e
Jordan Crouse <jcrouse@codeaurora.org>
2014-10-31 21:17:30 +0530

    msm: kgsl: Track the submitted timestamp for a draw context
    Track the last timestamp that each draw context has submitted to
    the ringbuffer.  This complete the picture for the draw context
    dump:
    
    kgsl-3d0: context[13]: queue=165, submit=115, start=101, retire=100
    
    here, timestamp 165-116 are on the queue, 115-102 are on the
    ringbuffer, 101 is in the GPU and 100 and older have been retired.
    If we get a fence timeout with this data we can easily see where the
    fence timestamp is in the pipeline.
    
    Change-Id: Ic0dedbadf54c1714296149af02b6938eef375d50
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -382,6 +382,7 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 	struct adreno_dispatcher *dispatcher = &adreno_dev->dispatcher;
 	int count = 0;
 	int requeued = 0;
+	unsigned int timestamp;
 
 	/*
 	 * Each context can send a specific number of command batches per cycle
@@ -424,6 +425,8 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 			continue;
 		}
 
+		timestamp = cmdbatch->timestamp;
+
 		ret = sendcmd(adreno_dev, cmdbatch);
 
 		/*
@@ -438,6 +441,8 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 				cmdbatch) ? 0 : 1;
 			break;
 		}
+
+		drawctxt->submitted_timestamp = timestamp;
 
 		count++;
 	}
@@ -721,6 +726,10 @@ int adreno_dispatcher_queue_cmd(struct adreno_device *adreno_dev,
 	}
 
 	cmdbatch->timestamp = *timestamp;
+
+	/* SYNC commands have timestamp 0 and will get optimized out anyway */
+	if (!(cmdbatch->flags & KGSL_CONTEXT_SYNC))
+		drawctxt->queued_timestamp = *timestamp;
 
 	/*
 	 * Set the fault tolerance policy for the command batch - assuming the
