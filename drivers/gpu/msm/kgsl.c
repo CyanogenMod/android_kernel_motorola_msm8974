1a9ec5031eb72c4761e58e8d5102ccf94089b279
Anupam Sakargayan <asakarga@codeaurora.org>
2014-12-04 15:47:43 +0530

    msm: kgsl: Device mutex lock for kgsl_cmdbatch_destroy
    kgsl_cancel_event must be called with device->mutex
    otherwise it will cause a BUG_ON in kgsl_cancel_event
    
    kgsl_cmdbatch_destroy will call kgsl_cancel_event, hence added
    device->mutex lock/unlock wherever kgsl_cmdbatch_destroy is called
    
    Change-Id: I9b9959236d6be0d80987279a64a0715363229f7b
    Signed-off-by: Anupam Sakargayan <asakarga@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2239,8 +2239,11 @@ free_cmdbatch:
 	 * -EPROTO is a "success" error - it just tells the user that the
 	 * context had previously faulted
 	 */
-	if (result && result != -EPROTO)
+	if (result && result != -EPROTO) {
+		kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 		kgsl_cmdbatch_destroy(cmdbatch);
+		kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
+	}
 
 done:
 	kgsl_context_put(context);
@@ -2291,8 +2294,11 @@ free_cmdbatch:
 	 * -EPROTO is a "success" error - it just tells the user that the
 	 * context had previously faulted
 	 */
-	if (result && result != -EPROTO)
+	if (result && result != -EPROTO) {
+		kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 		kgsl_cmdbatch_destroy(cmdbatch);
+		kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
+	}
 
 done:
 	kgsl_context_put(context);
