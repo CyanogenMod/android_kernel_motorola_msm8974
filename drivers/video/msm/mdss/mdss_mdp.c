ffb25685c0d4369a9c45a19fb99699771d5697ef
Christopher R. Palmer <crpalmer@gmail.com>
2015-02-22 05:38:35 -0500

    victara: mdss: Fix moto's version of ulps idle screen
    From:
    
      commit 1d8f5181869016846bad8147f5df4010cb895a23
      Author: Aravind Venkateswaran <aravindh@codeaurora.org>
      Date:   Thu Feb 20 16:19:40 2014 -0800
    
        msm: mdss: Turn off MDP GDSC in idle screen for cmd mode panels
    
    Change-Id: I39be6cd747ee4475588d12829dbcd7ed971136e0

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -2584,8 +2584,8 @@ static void mdss_mdp_footswitch_ctrl_locked(struct mdss_data_type *mdata,
 	if (on) {
 		if (!mdata->fs_ena) {
 			regulator_enable(mdata->fs);
-			mdss_mdp_cx_ctrl(mdata, true);
 			if (!mdata->ulps) {
+				mdss_mdp_cx_ctrl(mdata, true);
 				mdss_mdp_batfet_ctrl(mdata, true);
 			}
 		}
@@ -2600,8 +2600,8 @@ static void mdss_mdp_footswitch_ctrl_locked(struct mdss_data_type *mdata,
 		pr_debug("Disable MDP FS [%d]\n", mdata->fs_ena);
 		if (!mdata->fs_ena) {
 			regulator_disable(mdata->fs);
-			mdss_mdp_cx_ctrl(mdata, false);
 			if (!mdata->ulps) {
+				mdss_mdp_cx_ctrl(mdata, false);
 				mdss_mdp_batfet_ctrl(mdata, false);
 			}
 		}
