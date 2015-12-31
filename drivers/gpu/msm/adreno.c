e9b86443f4e17e76ef120b0a05f8b4a193c4893b
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-31 08:31:15 -0600

    victara: fix build when disabling DEBUG and DEBUG_FS
    Change-Id: I6a113b8e7c2f67d7ccd466dfdde2d652fa28d8c8

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1867,7 +1867,9 @@ adreno_probe(struct platform_device *pdev)
 	if (status)
 		goto error_close_device;
 
+#ifdef CONFIG_DEBUG_FS
 	adreno_debugfs_init(device);
+#endif
 	adreno_profile_init(device);
 
 	adreno_ft_init_sysfs(device);
